1dbcdbc2003a5bdceccd9a50cfd880d5c33f6607
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2018-01-25 19:24:35 +0100

    ListView with GestureRecognizer on item added

--- src/Madsense.XamarinForms.Sample/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/MainPageViewModel.cs
@@ -1,10 +1,25 @@
-﻿using System.ComponentModel;
+﻿using Xamarin.Forms;
+
+using System.Windows.Input;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Madsense.XamarinForms.Sample
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        public MainPageViewModel()
+        {
+            DetailCommand = new Command(Detail);
+        }
+
+        public ICommand DetailCommand { get; }
+
+        private async void Detail()
+        {
+            await Application.Current.MainPage.DisplayAlert("Click", "Detail on Item clicked", "OK");
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
