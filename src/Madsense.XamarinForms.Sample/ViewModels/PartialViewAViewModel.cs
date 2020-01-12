1e231864ded681448dcff03dc68cdd12bd2579f4
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-01-12 17:42:46 +0100

    PartialViewB added

--- src/Madsense.XamarinForms.Sample/ViewModels/PartialViewAViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/PartialViewAViewModel.cs
@@ -4,7 +4,7 @@ namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class PartialViewAViewModel
     {
-        public string Title => "Title from PartialViewAViewModel";
+        public string Title => "Title from PartialView A ViewModel";
         public string Id { get; } = Guid.NewGuid().ToString();
     }
 }
---
+++ src/Madsense.XamarinForms.Sample/ViewModels/PartialViewBViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Madsense.XamarinForms.Sample.ViewModels
+{
+    public class PartialViewBViewModel
+    {
+        public string Title => "Title from PartialView B ViewModel";
+        public string Id { get; } = Guid.NewGuid().ToString();
+    }
+}
