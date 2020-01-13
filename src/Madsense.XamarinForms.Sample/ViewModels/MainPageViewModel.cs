6af758954fbea83d0011aed28693445d86b29274
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-01-13 13:04:18 +0100

    Switch ControlTemplate on Command

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,35 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        private bool _isFirstTemplate = true;
+
+        public bool IsFirstTemplate
+        {
+            get => _isFirstTemplate;
+            set
+            {
+                if (_isFirstTemplate == value)
+                    return;
+
+                _isFirstTemplate = value;
+                OnPropertyChanged(nameof(IsFirstTemplate));
+            }
+        }
+
+        public ICommand SwitchTemplateCommand { get; }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            SwitchTemplateCommand = new Command(() => IsFirstTemplate = !IsFirstTemplate);
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
