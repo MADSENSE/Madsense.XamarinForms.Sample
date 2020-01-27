694d150800adaeb4efdb9dedd66c1412af5ec8f9
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-01-27 10:55:13 +0100

    CollectionView sample added

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>(Enumerable.Range(0, 200).Select(i => $"Item {i}"));
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
