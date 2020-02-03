9f2b424cceb49164aeb3ec621a2295678e961774
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-02-03 12:45:29 +0100

    cosmetics

--- Madsense.XamarinForms.Sample.Shared/AppManager.uwp.cs
+++ Madsense.XamarinForms.Sample.Shared/AppManager.uwp.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-using Windows.UI.Popups;
 using Windows.ApplicationModel.Core;
 
 namespace Madsense.XamarinForms.Sample
@@ -10,18 +9,8 @@ namespace Madsense.XamarinForms.Sample
     {
         public async Task<bool> TryRestartAsync()
         {
-            var result = await CoreApplication.RequestRestartAsync("Application Restart Programmatically ");
-
-            if (result == AppRestartFailureReason.NotInForeground ||
-                result == AppRestartFailureReason.RestartPending ||
-                result == AppRestartFailureReason.Other)
-            {
-                var msgBox = new MessageDialog("Restart Failed", result.ToString());
-                await msgBox.ShowAsync();
-                return false;
-            }
-
-            return true;
+            var _ = await CoreApplication.RequestRestartAsync("Application Restart Programmatically");
+            return false;
         }
     }
 }
