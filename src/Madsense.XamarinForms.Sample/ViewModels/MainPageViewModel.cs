c03890ddf66b4497b5540a721734d50e28bc9875
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-02-05 16:26:08 +0100

    webview with custom font added

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -5,7 +5,28 @@ namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        public string Html { get; }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            Html = 
+                "<html>" +
+                "" +
+                "<head>" +
+                "  <style type='text/css'>" +
+                "    @font-face { font-family: 'Roboto'; src: url('ms-appx-web:///Assets/Fonts/Roboto-Regular.ttf'); } " +
+                "    * { font-family: 'Roboto'; color: blue; } " +
+                "  </style>" +
+                "</head>" +
+                "" +
+                "<body>" +
+                "  <h1>Text in my Font</h1>" +
+                "</body>" +
+                "" +
+                "</html>";
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
