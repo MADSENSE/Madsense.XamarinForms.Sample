86c85462fe504cfdef93f162f6c22bb24a354d6d
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-02-23 13:32:59 +0100

    Load local image & font on WebView

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,28 +1,41 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
         public string Html { get; }
-
+        
         public event PropertyChangedEventHandler PropertyChanged;
 
         public MainPageViewModel()
         {
+            var fontFamily = "Bellada";
+            var fontFace = $"@font-face {{ font-family: '{fontFamily}'; src: url('bellada.ttf'); }}";
+
+            var imagePath = "madsense.png";
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                fontFace = $"@font-face {{ font-family: '{fontFamily}'; src: url('ms-appx-web:///Assets/Fonts/bellada.ttf'); }}";
+                imagePath = "ms-appx-web:///Assets/madsense.png";
+            }
+
             Html = 
                 "<html>" +
                 "" +
                 "<head>" +
                 "  <style type='text/css'>" +
-                "    @font-face { font-family: 'Roboto'; src: url('ms-appx-web:///Assets/Fonts/Roboto-Regular.ttf'); } " +
-                "    * { font-family: 'Roboto'; color: blue; } " +
+                $"    {fontFace} " +
+                $"    * {{ font-family: '{fontFamily}'; color: blue; }} " +
                 "  </style>" +
                 "</head>" +
                 "" +
                 "<body>" +
-                "  <h1>Text in my Font</h1>" +
+                "  <h1>HTML text with Bellada Font</h1>" +
+                $"  <img src='{imagePath}'>" +
                 "</body>" +
                 "" +
                 "</html>";
