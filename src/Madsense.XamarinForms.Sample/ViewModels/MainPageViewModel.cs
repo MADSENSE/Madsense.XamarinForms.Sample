cbd237046fb45f7377900b211de4ffde2d1cf792
Esteban Chornet Baides <esteban.chornet@madsense.com>
2020-07-28 10:01:35 +0200

    selected state bug

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
@@ -6,6 +8,15 @@ namespace Madsense.XamarinForms.Sample.ViewModels
     public class MainPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public string SelectedItem { get; set; }
+        public MvvmHelpers.ObservableRangeCollection<string> Items { get; } = new MvvmHelpers.ObservableRangeCollection<string>();
+
+        public MainPageViewModel()
+        {
+            Items.AddRange(new List<string> { "Apple", "Banana", "Watermelon", "Pineapple" });
+            SelectedItem = Items.First();
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
