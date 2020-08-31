0c40c9b1426e93e65c1439874c5c7a2a48500947
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-08-31 17:49:05 +0200

    basic viewmodel implementation added

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,27 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using MvvmHelpers;
+using Xamarin.Forms;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        public ObservableRangeCollection<string> Items { get; }
+
+        public ICommand AddItemCommand { get; }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            Items = new ObservableRangeCollection<string>();
+            AddItemCommand = new Command(() =>
+            {
+                Items.Add($"Item {Items.Count + 1}");
+            });
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
