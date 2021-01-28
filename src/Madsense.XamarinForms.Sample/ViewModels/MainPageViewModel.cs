61ef5cdbfaeb482279192ad650844ad784131f6c
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2021-01-28 09:03:51 +0100

    Binding change by DataTrigger sample added

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -5,6 +5,25 @@ namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        private string _selectedValue;
+
+        public string SelectedValue
+        {
+            get => _selectedValue;
+            set
+            {
+                if (_selectedValue == value)
+                    return;
+
+                _selectedValue = value;
+                OnPropertyChanged(nameof(SelectedValue));
+            }
+        }
+
+        public string Value1 { get; } = nameof(Value1);
+        public string Value2 { get; } = nameof(Value2);
+        public string Value3 { get; } = nameof(Value3);
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
