8be0ff2f4ca1f9d4a5d75d3228f4d774ebb1e186
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2021-01-28 09:06:26 +0100

    Value4 added

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -23,6 +23,7 @@ namespace Madsense.XamarinForms.Sample.ViewModels
         public string Value1 { get; } = nameof(Value1);
         public string Value2 { get; } = nameof(Value2);
         public string Value3 { get; } = nameof(Value3);
+        public string Value4 { get; } = nameof(Value4);
 
         public event PropertyChangedEventHandler PropertyChanged;
 
