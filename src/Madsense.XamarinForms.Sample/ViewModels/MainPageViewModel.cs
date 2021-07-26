fa9f53a4be16005b2257dd5c3886c942134e72cb
Esteban Chornet Baides <esteban.chornet@madsense.com>
2021-07-26 16:58:15 +0200

    added test view

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,37 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        private string _text;
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+
+        public ICommand ChangeLongTextCommand { get; }
+        public ICommand ChangeShortTextCommand { get; }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            var shortText = "Short text 12345";
+            Text = "Short text 12345";
+            ChangeLongTextCommand = new Command(() =>
+                Text = "This is a longer text to test that LineBreakMode is not working well");
+            ChangeShortTextCommand = new Command(() => Text = shortText);
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
