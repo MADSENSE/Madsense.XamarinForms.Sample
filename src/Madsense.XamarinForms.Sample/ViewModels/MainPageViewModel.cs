30698e8971c2eade32d39ef33eb35ebb9ab7e99a
Esteban Chornet Baides <esteban.chornet@madsense.com>
2021-07-28 10:23:23 +0200

    some more tests

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -8,6 +8,7 @@ namespace Madsense.XamarinForms.Sample.ViewModels
     public class MainPageViewModel : INotifyPropertyChanged
     {
         private string _text;
+        private bool _isVisible;
 
         public string Text
         {
@@ -19,18 +20,36 @@ namespace Madsense.XamarinForms.Sample.ViewModels
             }
         }
 
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
+
         public ICommand ChangeLongTextCommand { get; }
         public ICommand ChangeShortTextCommand { get; }
+        public ICommand HideLabelCommand { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public MainPageViewModel()
         {
             var shortText = "Short text 12345";
-            Text = "Short text 12345";
             ChangeLongTextCommand = new Command(() =>
-                Text = "This is a longer text to test that LineBreakMode is not working well");
-            ChangeShortTextCommand = new Command(() => Text = shortText);
+                {
+                    Text = "This is a longer text to test that LineBreakMode is not working well";
+                    IsVisible = true;
+                });
+            ChangeShortTextCommand = new Command(() =>
+            {
+                Text = shortText;
+                IsVisible = true;
+            });
+            HideLabelCommand = new Command(() => IsVisible = false);
         }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
