496acb5f52482649996cda95e0a25d14c90367bd
Yann Alex Zahringer Ferrando <yann.zahringer@madsense.com>
2021-10-20 08:58:55 +0200

    repro added

--- src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
+++ src/Madsense.XamarinForms.Sample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,33 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Madsense.XamarinForms.Sample.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        private bool _isVisible;
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                if (_isVisible == value)
+                    return;
+                _isVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ToggleVisibilityCommand { get; }
+
+        public MainPageViewModel()
+        {
+            ToggleVisibilityCommand = new Command(() => IsVisible = !IsVisible);
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
